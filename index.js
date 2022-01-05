4692f0c06e78145ac6e7be7c3d20745b0449b3e5
Precious Ibeagi <preciousaang@gmail.com>
2022-01-05 23:31:08 +0100

    added dotenv to app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
